dd1c9e1ee3addb4fe9a025e4aaa21f106f66880e
maximus <maximusagub@gmail.com>
2022-10-18 12:22:45 -0700

    working timer countdown

--- src/components/navbar/clock/clock.js
+++ src/components/navbar/clock/clock.js
@@ -1,43 +1,68 @@
 import React, { Component, useEffect, useState } from "react";
 
-
 const Clock = () => {
-  const [seconds, setSeconds] = useState(0)
-  const [minuets, setMinuets] = useState(0);
+  const [seconds, setSeconds] = useState(59);
+  const [minuets, setMinuets] = useState(20);
+  const [duration, setDuration] = useState("20:00");
+  const [isStart, setStart] = useState(false);
+
+  const onStartTimer = (e) => {
+    e.preventDefault();
+    setStart(true);
+  }
+
   var timer;
-    useEffect(()=>{
+  useEffect(() => {
+    if (isStart) {
       timer = setInterval(() => {
-        setSeconds(seconds+1);
-        if (seconds===59) {
-          setMinuets(minuets+1);
-          setSeconds(0)
+        setSeconds(seconds - 1);
+        if (seconds === 0) {
+          setMinuets(minuets - 1);
+          setSeconds(59);
         }
       }, 1000);
-      return () => clearInterval(timer)
-    })
-
-    const timer_restart =()=> {
-      setSeconds(0);
-      setMinuets(0);
+      return () => clearInterval(timer);
     }
+  });
 
-    const timer_stop =()=> {
-      clearInterval(timer)
-    }
+  const timer_restart = () => {
+    setSeconds(59);
+    setMinuets(20);
+  };
 
-    return (
-      <div className="timer">
-        <div className="container">
-          <div className="timer_container">
-            <h1>
-             {minuets < 10 ? "0" + minuets : minuets} : {seconds < 10 ? "0" + seconds : seconds}
-            </h1>
-            <button className="timer_start" onClick={timer_restart}> Restart </button>
-            <button className="timer_stop" onClick={timer_stop}> Stop </button>
-          </div>
+  const timer_stop = () => {
+    clearInterval(timer);
+  };
+
+  return (
+    <div className="timer">
+      <div className="container">
+        <div className="timer_container">
+          <h1>
+            {minuets < 10 ? "0" + minuets : minuets} :{" "}
+            {seconds < 10 ? "0" + seconds : seconds}
+          </h1>
+          <form>
+            <label>Study Duration: </label>
+            <select
+              value={duration}
+              onChange={(e) => {
+                setDuration(e.target.value);
+              }}
+            >
+              <option value="10:00"> 10:00 </option>
+              <option value="20:00"> 20:00 </option>
+              <option value="30:00"> 30:00 </option>
+            </select>
+          </form>
+          
+          <button className="timer_start" onClick={(e) => onStartTimer(e)}> Start </button>
+          <button className="timer_restart" onClick={timer_restart}> Restart </button>
+          <button className="timer_stop" onClick={timer_stop}> Stop </button>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Clock
+export default Clock;
