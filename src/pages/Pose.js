afcb08f0d7ac58fb2e2c08cb3333806a30336a03
maximus <maximusagub@gmail.com>
2022-11-03 21:15:48 -0700

    working mvp

--- src/pages/Pose.js
+++ src/pages/Pose.js
@@ -14,101 +14,115 @@ import * as PoseNet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
 import './page.css';
 import { drawMesh } from "../utilitis.js";
-import { div } from "@tensorflow/tfjs";
+import { div, time } from "@tensorflow/tfjs";
 
 function Pose() {
-
+  
   const [seconds, setSeconds] = useState(5); // 0
   const [minuets, setMinuets] = useState(0); // 20
   const [duration, setDuration] = useState("00:05"); // "20:00"
   const [isStart, setStart] = useState(false);
   const [isDone, setStatus] = useState(false); // false (not done) true (done)
+  const [neg, setNeg] = useState(0);
+  const [pos, setPos] = useState(0);
+  
 
+  
+  // this msg is used at the bottom of the view to 
+  
+  var infoT1;
+  var infoT2;
+  
+  
+  if (seconds === 0 && minuets ===0 ) {
+    infoT1 = "attention points - " + pos;
+    infoT2 = "non attentive points - " + neg;
+  } else {
+    infoT1 = "Hey, we will let you know how well your attention rate was at the"
+    infoT2 = "at the end of the timer."
+  } 
+  
   const onStartTimer = (e) => {
     e.preventDefault();
     setStart(true);
     runPosenet();
   }
-
+  
   var timer;
-
+  
   useEffect( () =>  {
     
     if (isStart) {
       setStatus(false);
       timer = setInterval(() => {
-      // The logic here is that the intervol can keep running but
-      // we only want the timer countdown to run if useState of setStatus is set to true
-      if (!isDone) {
-        setSeconds(seconds - 1);
-        if (seconds === 0) {
-          setMinuets(minuets - 1);
+        // The logic here is that the intervol can keep running but
+        // we only want the timer countdown to run if useState of setStatus is set to true
+        if (!isDone) {
+          if (seconds === 0 && minuets === 0) {
+            return clearInterval(timer)
+          }
+          setSeconds(seconds - 1);
+          if (seconds === 0) {
+            setMinuets(minuets - 1);
           setSeconds(59);
 
           // if the seconds is divisibale by 5 then run our net modal (from func runPosenet)
           if (seconds % 5 === 0) {
             runPosenet();
           }
-          // if time is equal to 00:00 then stop the timer and display results
-          if (minuets === 0 && seconds === 0) {
-            console.log("we should stop now");
-            setStatus(true); // isDone is now true so stop the seconds and minuets adjustments.
-          }
-
         }
       }
       
-      }, 1000);
-     return () => clearInterval(timer);
-    }
+    }, 1000);
+    return () => clearInterval(timer);
+  }
+});
+
+const timer_restart = () => {
+  setSeconds(0);
+  setMinuets(20);
+  
+};
+
+const timer_stop = () => {
+  clearInterval(timer);
+};
+
+
+const webcamRef = useRef(null);
+const canvasRef = useRef(null);
+
+// Load posenet
+const runPosenet = async () => {
+  var net = await PoseNet.load({
+    inputResolution: { width: 640, height: 480 },
+    scale: 0.5,
   });
+  
+  detect(net);
 
-  const timer_restart = () => {
-    setSeconds(0);
-    setMinuets(20);
+};
 
-  };
-
-  const timer_stop = () => {
-    clearInterval(timer);
-  };
-
-
-  var rate = { neg: 0, pos: 0 };
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
-
-  // Load posenet
-  const runPosenet = async () => {
-   var net = await PoseNet.load({
-     inputResolution: { width: 640, height: 480 },
-     scale: 0.5,
-   });
-
-   detect(net);
-
-  };
-
-  // Detect function
-  const detect = async (net) => {
-    try {
-      if (
-        typeof webcamRef.current !== "undefined" &&
-        webcamRef.current !== null &&
-        webcamRef.current.video.readyState === 4
+// Detect function
+const detect = async (net) => {
+  try {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
       ) {
         // get vdeo properties
         const video = webcamRef.current.video;
         const videoWidth = webcamRef.current.video.videoWidth;
         const videoHeight = webcamRef.current.video.videoHeight;
-  
+        
         // set the video width
         webcamRef.current.video.width = videoWidth;
         webcamRef.current.video.height = videoHeight;
-  
+        
         // Make directions
         const face = await net.estimateSinglePose(video);
-  
+        
         drawCanvas(face, video, videoWidth, videoHeight, canvasRef);
       }
     } catch (error) {
@@ -117,20 +131,27 @@ function Pose() {
       console.log("${error}")
     }
   };
-
+  
   // drawig on the canvas
   const drawCanvas = (face, video, videoWidth, videoHeight, canvas) => {
     const ctx = canvas.current.getContext("2d");
     canvas.current.width = videoWidth;
     canvas.current.height = videoHeight;
     if (face["keypoints"][0]["score"] < 0.8) {
-      rate["neg"] += 1;
-      console.log(rate);
+
+      setNeg(neg + 1);
+      
+      console.log(neg);
     } else {
-      rate["pos"] += 1;
-      console.log(rate);
+      setPos(pos + 1);
+      
+      console.log(pos);
     }
+    
     console.log(face["keypoints"][0]["score"]); // there is a value of face["keypoints"][idx]["score"]
+    
+    
+    
     // drawMesh(face, ctx);
   };
   
@@ -195,8 +216,8 @@ function Pose() {
             right: 0,
             textAlign: "center",
             zIndex: 9,
-            width: 640,
-            height: 480,
+            width: 440,
+            height: 280,
           }}
         />
 
@@ -211,11 +232,17 @@ function Pose() {
             right: 0,
             textAlign: "center",
             zIndex: 9,
-            width: 640,
-            height: 480,
+            width: 440,
+            height: 280,
           }}
         />
       </header>
+    
+      <div className="infoSpan"> 
+        <span> {infoT1}</span>
+        <span> {infoT2} </span>
+      </div>
+
     </div>
   );
 }
