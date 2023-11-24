2dc68d22dac471c609934e24e1448df6f0be52bf
Maximus <max@Maximuss-MacBook-Air.local>
2023-11-24 09:17:51 -0800

    moving to win for api work

--- src/pagesNew/FindFaces.js
+++ src/pagesNew/FindFaces.js
@@ -1,4 +1,12 @@
 import Navbar from "../components/Navbar"
+// need to wrap code lab in a node node express
+// need to call node express from this UI
+// awiat the imag data as bytes
+// recieve the image data as bytes
+// display a list of cropped faces
+
+// add a research section and describe findings with similar models
+
 export default function () {
     return (
         <div>
